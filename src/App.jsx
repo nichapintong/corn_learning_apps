3d110713cd5355ae422ee86c862d5513446d986e
nichapintong <beernicha47@gmail.com>
2025-09-04 11:56:33 +0700

    04092025 - 1

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ function App() {
 
       const res = await axios.post(
         // "http://127.0.0.1:5000/predict",
-        "https://api.3.27.181.193.sslip.io/predict",
+        "https://api.3.27.202.55.sslip.io/predict",
         fd,
         { timeout: 120000 }
       );
